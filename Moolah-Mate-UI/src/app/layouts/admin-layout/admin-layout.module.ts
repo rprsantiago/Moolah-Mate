344ab95f8b2ed04c9ba82fd84c8bd6cbcc544654
rprsantiago <ronnelpaulosantiago@gmail.com>
2024-01-22 20:20:11 +0800

    Made changes to the template
    1. Removed Icons, Maps and Upgrade components from the UI
    2. Added a new icon for the app.
    3. Updated footer

--- Moolah-Mate-UI/src/app/layouts/admin-layout/admin-layout.module.ts
+++ Moolah-Mate-UI/src/app/layouts/admin-layout/admin-layout.module.ts
@@ -7,10 +7,7 @@ import { DashboardComponent } from '../../dashboard/dashboard.component';
 import { UserProfileComponent } from '../../user-profile/user-profile.component';
 import { TableListComponent } from '../../table-list/table-list.component';
 import { TypographyComponent } from '../../typography/typography.component';
-import { IconsComponent } from '../../icons/icons.component';
-import { MapsComponent } from '../../maps/maps.component';
 import { NotificationsComponent } from '../../notifications/notifications.component';
-import { UpgradeComponent } from '../../upgrade/upgrade.component';
 import {MatButtonModule} from '@angular/material/button';
 import {MatInputModule} from '@angular/material/input';
 import {MatRippleModule} from '@angular/material/core';
@@ -39,10 +36,7 @@ import { FinancialRecordsComponent } from 'app/financial-records/financial-recor
     UserProfileComponent,
     TableListComponent,
     TypographyComponent,
-    IconsComponent,
-    MapsComponent,
     NotificationsComponent,
-    UpgradeComponent,
     AccountsComponent,
     FinancialRecordsComponent
   ]
